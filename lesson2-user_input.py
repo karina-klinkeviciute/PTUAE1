78cad12ef8df2d9e46f0402de3826dbc12a7ff44
karina <karina.klinkeviciute@.>
2023-12-19 20:27:36 +0200

    user input

---
+++ lesson2-user_input.py
@@ -0,0 +1,13 @@
+# ask for age and name and print them
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(name)
+print(age)
+
+# ask for two numbers, then multiply them, and print the result
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+number3 = number1 * number2
+
+print(number3)

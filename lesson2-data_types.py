7cf8a0f977f8aeb9fecac3badd99b78874e084df
karina <karina.klinkeviciute@.>
2023-12-19 18:24:06 +0200

    strings and joining them together

--- lesson2-data_types.py
+++ lesson2-data_types.py
@@ -1,5 +1,5 @@
-a = 9
-b = 9
+a = 2.8
+b = 9.3
 
 c = a + b
 
@@ -9,9 +9,48 @@ e = a * b
 
 f = a / b
 
+g = a // b
+
+h = a % b
+
+i = a ** b
+
+
 print(c)
 print(d)
 print(e)
 print(f)
+print(g)
+print(h)
+print(i)
 
 print(c, d, e, f)
+
+# String type operations
+
+letter = "a"
+
+letter2 = "b"
+
+name = "Karina"
+
+company = "Code Academy"
+
+sentence = "I really enjoy learning python !"
+
+age = "55 years"
+
+age2 = "9 months"
+
+print(company)
+
+name_company = name + " works at " + company
+
+print(name_company)
+
+# f stands for "formated"
+#  we add variables in curly braces: {}
+# f - string is the most common method of formatting
+name_company2 = f"{name} works at {company}"
+
+print(name_company2)

12758004151c4b597ce5ad199943b9cfa1d6f9ea
karina <karina.klinkeviciute@.>
2023-12-20 19:54:32 +0200

    comments

--- lesson3-lists_loops.py
+++ lesson3-lists_loops.py
@@ -2,9 +2,15 @@ list = [1, 5, 9, 7, 5, 3, 2, 4, 8, 6, 12, 65, 78]
 
 list2 = [1, 2, 3, 4]
 
+# adding somethign to each value
 for item in list:
-    print(item * item)
+    print(item + 5)
 
+# converting to a string and adding another string
+for item in list:
+    print(str(item) + " EUR")
+
+# inserting an item from one list into specific place in another list
 for item in list2:
     list.insert(3, item)
 

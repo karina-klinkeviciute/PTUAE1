fcbf3186c779520be88b1475a6be9aba3f080705
karina <karina.klinkeviciute@.>
2023-12-21 21:22:46 +0200

    exercise 3

---
+++ exercise.py
@@ -0,0 +1,19 @@
+sentence_length = 4
+sentences = []
+for i in range(4):
+    sentence = input("Enter you sentence").lower()
+    sentences.append(sentence)
+    print(sentence)
+
+character_counts = {}
+for sentence in sentences:
+    for character in sentence:
+        # character_counts[character] = character_counts.get(character, 0) + 1
+# print(character_counts)
+
+        if character in character_counts.keys():
+            character_counts[character] += 1
+        else:
+            character_counts[character] = 1
+
+print(character_counts)

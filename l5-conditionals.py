0fab98a4e273447407b1759e52cddc0cec07be8b
karina <karina.klinkeviciute@.>
2023-12-27 20:06:13 +0200

    more conditionals

--- l5-conditionals.py
+++ l5-conditionals.py
@@ -1,46 +1,79 @@
-number1 = int(input("enter number1 "))
-number2 = int(input("enter number2 "))
-
-#  comparison of numbers
-# if number1 > number2:
-#     print("more")
-# elif number1 < number2:
-#     print("less")
+# number1 = int(input("enter number1 "))
+# number2 = int(input("enter number2 "))
+#
+# #  comparison of numbers
+# # if number1 > number2:
+# #     print("more")
+# # elif number1 < number2:
+# #     print("less")
+# # else:
+# #     print("equal")
+#
+# year = int(input("enter year of birth: "))
+#
+#
+# # lots of elif
+# if year >= 2010:
+#     print("generation Alpha")
+# elif year >= 1997:
+#     print("generation Z")
+# elif year >= 1981:
+#     print("Millenial")
+# elif year >= 1965:
+#     print("Generation X")
+# elif year >= 1946:
+#     print("Baby Boomer")
+# elif year >= 1928:
+#     print("Silent generation")
 # else:
-#     print("equal")
+#     print("some earlier generation")
+#
+# # shorter if (though less readable
+# print("number1 is bigger") if number1 > number2 else print("number2 is bigger") if number2 > number1 else print("they are equal")
+#
+# a = 200
+# b = 300
+# c = 100
+#
+# # checking two conditions
+# if c > b and c > a:
+#     print("number c is larger than the other ones")
+#
+# if c > b or c > a:
+#     print("number c is larger then at least on of other numbers")
+# else:
+#     print("number c is not larger then at least on of other numbers")
 
-year = int(input("enter year of birth: "))
+# x = 35
+#
+# if x > 10:
+#     print("x is more than 10")
+#     if x > 20:
+#         print("x is more than 20")
+#         if x > 30:
+#             pass
+#         else:
+#             print("x is between 20 and 30")
 
+name = input("tell me your name: ")
 
-# lots of elif
-if year >= 2010:
-    print("generation Alpha")
-elif year >= 1997:
-    print("generation Z")
-elif year >= 1981:
-    print("Millenial")
-elif year >= 1965:
-    print("Generation X")
-elif year >= 1946:
-    print("Baby Boomer")
-elif year >= 1928:
-    print("Silent generation")
+if name == "Tom":
+    print("Hi, Tom")
 else:
-    print("some earlier generation")
+    print("good bye")
 
-# shorter if (though less readable
-print("number1 is bigger") if number1 > number2 else print("number2 is bigger") if number2 > number1 else print("they are equal")
+admins = ["Tom", "John", "Gary"]
 
-a = 200
-b = 300
-c = 100
-
-# checking two conditions
-if c > b and c > a:
-    print("number c is larger than the other ones")
-
-if c > b or c > a:
-    print("number c is larger then at least on of other numbers")
+if name in admins:
+    print("You have full administrative rights")
 else:
-    print("number c is not larger then at least on of other numbers")
+    print("You have regular user rights")
 
+if not name:
+    print("you have to enter a name")
+
+
+item_count = 0
+
+if not item_count:
+    print("no items")

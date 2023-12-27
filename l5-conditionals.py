d0bf0722c241b3c13d08b525c6dab6a7ec59f24c
karina <karina.klinkeviciute@.>
2023-12-27 19:28:14 +0200

    more conditionals

--- l5-conditionals.py
+++ l5-conditionals.py
@@ -1,7 +1,42 @@
 number1 = int(input("enter number1 "))
 number2 = int(input("enter number2 "))
 
-if number1 > number2:
-    print("more or equal")
+#  comparison of numbers
+# if number1 > number2:
+#     print("more")
+# elif number1 < number2:
+#     print("less")
+# else:
+#     print("equal")
+
+year = int(input("enter year of birth: "))
+
+if year >= 2010:
+    print("generation Alpha")
+elif year >= 1997:
+    print("generation Z")
+elif year >= 1981:
+    print("Millenial")
+elif year >= 1965:
+    print("Generation X")
+elif year >= 1946:
+    print("Baby Boomer")
+elif year >= 1928:
+    print("Silent generation")
 else:
-    print("less")
+    print("some earlier generation")
+
+print("number1 is bigger") if number1 > number2 else print("number2 is bigger") if number2 > number1 else print("they are equal")
+
+a = 200
+b = 300
+c = 100
+
+if c > b and c > a:
+    print("number c is larger than the other ones")
+
+if c > b or c > a:
+    print("number c is larger then at least on of other numbers")
+else:
+    print("number c is not larger then at least on of other numbers")
+

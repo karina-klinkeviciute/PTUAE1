f67737277883ad1e713742a5c28f0a360d95ab6e
karina <karina.klinkeviciute@.>
2023-12-28 18:56:12 +0200

    some built in functions

---
+++ l6-print.py
@@ -0,0 +1,37 @@
+# print("Hello World")
+# print("Hi there")
+#
+# for a in range(7):
+#     for b in range(a):
+#         print("*", end=" ")
+#     print()
+
+print("hello", "world")
+
+print(*["hello", "world"])
+
+print("hello", "world", sep=",")
+
+print("hello", "world", sep=" amazing ")
+
+print(type("hi there"))
+
+print(type(15))
+
+print(type([1, 2, 3]))
+
+sentence = "hi there, how are you?"
+
+print(len(sentence))
+
+words = sentence.split()
+
+parts = sentence.split(",")
+
+print(parts)
+
+print(type(words))
+
+print(len(words))
+
+print((len(parts)))

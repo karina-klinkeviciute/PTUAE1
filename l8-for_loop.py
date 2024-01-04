7107d9489933d9326f75df2b31e63bf324984722
karina <karina.klinkeviciute@.>
2024-01-04 19:35:21 +0200

    loops

---
+++ l8-for_loop.py
@@ -0,0 +1,23 @@
+# names = ["Albert", "Tom", "Leonardo"]
+#
+# for name in names:
+#     print(f"Hello, {name}")
+
+# name = "Code Academy"
+# for character in name:
+#     print(character)
+
+# names = {"Albert", "Tom", "Leonardo"}
+#
+# for name in names:
+#     print(f"Hello, {name}")
+
+# my_dictionary = {"name": "Albert", "role": "scientist"}
+#
+# for key, value in my_dictionary.items():
+#     print(key, value)
+
+x = range(200, 100, -11)
+
+for item in x:
+    print(item)

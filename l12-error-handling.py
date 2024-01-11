38ee1e2c3d447b7d862daecbb7afdb6a24786a0f
karina <karina.klinkeviciute@.>
2024-01-11 18:42:29 +0200

    multiple exceptions

--- l12-error-handling.py
+++ l12-error-handling.py
@@ -1,13 +1,17 @@
 
-list1 = [12, 5, 3, 4, 56, 12]
+list1 = [12, 5, 3, 4, 56, 12, 58, 45]
 
-list2 = [0, 2, 3, 0, 12, 78]
+list2 = [0, 2, 3, 0, "12", 78]
 
 for index, item in enumerate(list1):
     try:
         result = item/list2[index]
+        print(result)
     except ZeroDivisionError:
-        result = "infinity"
+        print("infinitty")
+    except TypeError:
+        print("cant divide int by string")
+    except IndexError:
+        print("too many items")
 
-    print(result)
 

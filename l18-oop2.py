ff39d84e2c4387c595e7904b16e2c6da853a3a4e
karina <karina.klinkeviciute@.>
2024-01-18 19:00:58 +0200

    multi inheritance

--- l18-oop2.py
+++ l18-oop2.py
@@ -17,9 +17,32 @@ class Engineer(Employee):
     def print_data(self):
         print(self.level)
 
+class Person:
+    def __init__(self, hobbies):
+        self.hobbies = hobbies
+
+    def show_hobbies(self):
+        print(self.hobbies)
+
+class Designer(Employee, Person):
+    def __init__(self, name, age, experience, salary, position, hobbies):
+        Employee.__init__(self, name, age, experience, salary)
+        Person.__init__(self, hobbies)
+        self.position = position
+
+    def show(self):
+        super().show()
+        print(self.position)
+
+
 engineer = Engineer("John", 52, 20, 5000, "Senior developer")
 
 engineer.show()
 
 engineer.print_data()
 
+designer = Designer("Tom", 54, 10, 4000, "UI designer", "skiing")
+
+designer.show()
+
+designer.show_hobbies()

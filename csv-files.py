3532e4ae4b2fc5d4870f1b1f20eb966d9c87009a
karina <karina.klinkeviciute@.>
2024-03-05 21:56:45 +0200

    more on CSV files

--- csv-files.py
+++ csv-files.py
@@ -1,10 +1,21 @@
-# import csv
+
+import csv
+
+# with open("csv_sales_data.csv", newline="") as csvfile:
+#     sales_data = csv.reader(csvfile)
 #
-# with open('names.csv', 'w', newline='') as csvfile:
-#     fieldnames = ['first_name', 'last_name']
-#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#
-#     writer.writeheader()
-#     writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
-#     writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
-#     writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+#     for row in sales_data:
+#         for item in row:
+#             print(item, end=" ")
+#         print()
+
+
+with open("csv_people_data.csv", "w", newline="") as csvfile:
+    data_writer = csv.writer(csvfile)
+    data_writer.writerow(["Tom", "Kaunas", "25"])
+    data_writer.writerow(["John", "London", "46"])
+
+with open("csv_people_data.csv", "w", newline="") as csvfile:
+    data_writer = csv.writer(csvfile, delimiter=" ", quotechar='|')
+    data_writer.writerow(["Tom", "Kaunas", "25"])
+    data_writer.writerow(["John", "London", "46"])
